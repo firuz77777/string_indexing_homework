199e6b7cd9ef555dc908e0a540c36e11214e0c0d
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-11-01 20:44:36 +0500

    desyat

--- Index10.py
+++ Index10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x1 = s[0]
+    x2 = s[1]
+    x3 = s[2]
+    x4 = s[3]
+    x5 = s[4]
+    a = f'{x1+x2+x3+x4+x5}'
+    return a
+print(main("23532"))
